a954a15c8016d2beabd1a13bf0a2445389a32453
andrw3000 <a.j.corbett@exeter.ac.uk>
2021-12-09 10:57:58 +0000

    Line segmentation

--- lines/hough_lines.py
+++ lines/hough_lines.py
@@ -3,18 +3,79 @@ from skimage.transform import hough_line
 from skimage.transform import hough_line_peaks
 from skimage.draw import line
 
+
+def boundary_intersections(angle, dist):
+    """Converts Hough angle + distance to (x, y) axis instercepts.
+
+    Returns: [tuple] (y intercept, x intercept)
+    """
+    x0 = np.rint(dist / np.cos(angle))
+    y0 = np.rint(dist / np.sin(angle))
+    return (0, y0), (x0, 0)
+
+
+def weighted_line(r0, c0, r1, c1, w, rmin=0, rmax=np.inf):
+    # The algorithm below works fine if c1 >= c0 and c1-c0 >= abs(r1-r0).
+    # If either of these cases are violated, do some switches.
+    if abs(c1-c0) < abs(r1-r0):
+        # Switch x and y, and switch again when returning.
+        xx, yy, val = weighted_line(c0, r0, c1, r1, w, rmin=rmin, rmax=rmax)
+        return yy, xx, val
+
+    # At this point we know that the distance in columns (x) is greater
+    # than that in rows (y). Possibly one more switch if c0 > c1.
+    if c0 > c1:
+        return weighted_line(r1, c1, r0, c0, w, rmin=rmin, rmax=rmax)
+
+    # The following is now always < 1 in abs
+    slope = (r1-r0) / (c1-c0)
+
+    # Adjust weight by the slope
+    w *= np.sqrt(1+np.abs(slope)) / 2
+
+    # We write y as a function of x, because the slope is always <= 1
+    # (in absolute value)
+    x = np.arange(c0, c1+1, dtype=float)
+    y = x * slope + (c1*r0-c0*r1) / (c1-c0)
+
+    # Now instead of 2 values for y, we have 2*np.ceil(w/2).
+    # All values are 1 except the upmost and bottommost.
+    thickness = np.ceil(w/2)
+    yy = (np.floor(y).reshape(-1, 1)
+          + np.arange(-thickness-1, thickness+2).reshape(1, -1))
+    xx = np.repeat(x, yy.shape[1])
+
+    # Compute trapez
+    y0 = y.reshape(-1, 1)
+    trapez = np.clip(
+        np.minimum(yy + 1 + w / 2 - y0, -yy + 1 + w / 2 + y0), 0, 1
+    )
+    vals = trapez.flatten()
+
+    yy = yy.flatten()
+
+    # Exclude useless parts and those outside of the interval
+    # to avoid parts outside of the picture
+    mask = np.logical_and.reduce((yy >= rmin, yy < rmax, vals > 0))
+
+    return yy[mask].astype(int), xx[mask].astype(int), vals[mask]
+
+
 def mask2lines(mask: np.ndarray):
     """Traces straight lines through semantic filament segmentations."""
 
     # Classic straight-line Hough transform with .5 degree precision
     tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint=False)
-    h, theta, d = hough_line(mask, theta=tested_angles)
+    h, angles, dists = hough_line(mask, theta=tested_angles)
+    xint = []
+    yint = []
+    for _, angle, dist in zip(*hough_line_peaks(
+            h, angles, dists, min_distance=100, min_angle=10
+    )):
+        xaxis, yaxis = boundary_intersections(angle, dist)
+        xint.append(xaxis)
+        yint.append(yaxis)
 
-    angle_step = 0.5 * np.diff(theta).mean()
-    d_step = 0.5 * np.diff(d).mean()
-    bounds = [np.rad2deg(theta[0] - angle_step),
-              np.rad2deg(theta[-1] + angle_step),
-              d[-1] + d_step, d[0] - d_step]
 
     # rr, cc = line(1, 1, 8, 8)
 
--- lines/line_detection.py
+++ lines/line_detection.py
@@ -12,8 +12,8 @@ from matplotlib import cm
 
 
 # Import test image
-root_dir = '/'
-data_dir = os.path.join(root_dir, '../data/masks-tf1/semantic')
+root_dir = '/Users/Holmes/Research/IDSAI/PROOF/filament-segmentation'
+data_dir = os.path.join(root_dir, 'data/masks-tf1/semantic')
 image_file = 'tf1_001.png'
 image = io.imread(os.path.join(data_dir, image_file))
 if len(image.shape) > 2:
@@ -28,7 +28,7 @@ edges = canny(image, 2, 1, 25)
 print('edges shape: ', edges.shape)
 
 tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint=False)
-h, theta, d = hough_line(edges, theta=tested_angles)
+h, theta, d = hough_line(image, theta=tested_angles)
 
 print('h.shape: ', h.shape)
 print('theta.shape: ', theta.shape)
@@ -38,50 +38,17 @@ print('d.shape: ', d.shape)
 fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 ax = axes.ravel()
 
-ax[0].imshow(edges) #, cmap=cm.gray)
+ax[0].imshow(image, cmap=cm.gray)
 ax[0].set_title('Input image')
 
-ax[1].imshow(edges, cmap=cm.gray)
+ax[1].imshow(image, cmap=cm.gray)
 ax[1].set_ylim((image.shape[0], 0))
 ax[1].set_xlim((0, image.shape[1]))
 ax[1].set_title('Detected lines')
 
-for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
+for _, angle, dist in zip(*hough_line_peaks(h, theta, d, min_distance=50)):
     (x0, y0) = dist * np.array([np.cos(angle), np.sin(angle)])
     ax[1].axline((x0, y0), slope=np.tan(angle + np.pi/2))
 
 plt.tight_layout()
 plt.show()
-
-prob = False
-if prob:
-    # Line finding using the Probabilistic Hough Transform
-    edges = canny(image, 2, 1, 25)
-    lines = probabilistic_hough_line(edges,
-                                     threshold=10,
-                                     line_length=5,
-                                     line_gap=3)
-
-    # Generating figure 2
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharex=True, sharey=True)
-    ax = axes.ravel()
-
-    ax[0].imshow(image, cmap=cm.gray)
-    ax[0].set_title('Input image')
-
-    ax[1].imshow(edges, cmap=cm.gray)
-    ax[1].set_title('Canny edges')
-
-    ax[2].imshow(edges * 0)
-    for line in lines:
-        p0, p1 = line
-        ax[2].plot((p0[0], p1[0]), (p0[1], p1[1]))
-    ax[2].set_xlim((0, image.shape[1]))
-    ax[2].set_ylim((image.shape[0], 0))
-    ax[2].set_title('Probabilistic Hough')
-
-    for a in ax:
-        a.set_axis_off()
-
-    plt.tight_layout()
-    plt.show()
