cd135c79903c6563b60706336b9c52257dc7a15a
andrw3000 <a.j.corbett@exeter.ac.uk>
2022-01-18 09:52:58 +0000

    STAR file functionality

--- utils/coords2star.py
+++ utils/coords2star.py
@@ -16,46 +16,30 @@ def coords2star(line_ends, star_dir):
     """
 
     # Generate blank data frame with the headers
-    df = pd.DataFrame(columns=['Xcoords', 'Ycoords', 'ImageName'])
+    df = pd.DataFrame(columns=['Xstart', 'Xstop',
+                               'Ystart', 'Ystop',
+                               'Line Number'])
 
-    # Check line ends
+    # Row labels
+    filament = 'Filament {:02d}'
+
+    # Extract line ends and append to df
     for idx, end_pairs in enumerate(line_ends):
-        print('\nLine endings on line {}:'.format(idx + 1))
-        for ends in end_pairs:
-            e1 = ends[0]
-            e2 = ends[1]
-            print('({e1y:d}, {e1x:d}) -> ({e2y:d}, {e2x:d})'.format(
-                e1y=int(e1[0]), e1x=int(e1[1]), e2y=int(e2[0]), e2x=int(e2[1])
-            ))
+        for line_num, ends in enumerate(end_pairs):
+            e1 = ends[0]  # Start coords
+            e2 = ends[1]  # Stop coords
+            new_values = {'Xstart': int(e1[1]),
+                          'Xstop': int(e2[1]),
+                          'Ystart': int(e1[0]),
+                          'Ystop': int(e2[0]),
+                          'Line Number': line_num + 1,
+                          }
+            next_row = pd.Series(new_values, name=filament.format(idx + 1))
+            df = df.append(next_row)
 
-    i = 0
-
-    # for each model file, convert to txt using model2point,
-    # load into a pandas dataframe and append with tomogram name in a 4th column
-    while i < len(modlist):
-        print('model2point -sc -input ' + str(modlist[i]) + ' -ou ' + str(
-            modlist[i]) + '.txt')
-        os.system('model2point -sc -input ' + str(modlist[i]) + ' -ou ' + str(
-            modlist[i]) + '.txt')
-        modfile = pd.read_csv(modlist[i] + '.txt', sep=' ', header=None,
-                              skipinitialspace=True)
-        modfile.columns = ['rlnCoordinateX', 'rlnCoordinateY',
-                           'rlnCoordinateZ']
-        path = Path(edf[i])
-        # .stem gives root file name
-        file = Path(edf[i]).stem
-        modfile.insert(3, 'rlnMicrographName',
-                       './' + file + '/' + file + '.tomostar')
-        star_file = star_file.append(modfile)
-        i += 1
-
-    starfile.write(star_file, star_dir)
+    starfile.write(df, star_dir)
 
     # Print first few lines of the STAR file for sanity
     with open(star_dir) as file:
         for line in islice(file, 15):
             print(line)
-
-
-
-
